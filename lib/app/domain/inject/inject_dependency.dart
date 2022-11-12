9a6cf512a9ec1a40105b08060b4f133311695a36
Kevin-Sarges <kevinsarges08@gmail.com>
2022-11-11 21:04:02 -0300

    add inject para o login

--- lib/app/domain/inject/inject_dependency.dart
+++ lib/app/domain/inject/inject_dependency.dart
@@ -2,6 +2,7 @@ import 'package:desafio_apirest/app/data/datasoucer/cart_interface.dart';
 import 'package:desafio_apirest/app/data/services/cart/sqflite_service.dart';
 import 'package:desafio_apirest/app/presenter/view/cart/controller_cart/cart_controller.dart';
 import 'package:desafio_apirest/app/presenter/view/home/controller_home/home_controller.dart';
+import 'package:desafio_apirest/app/presenter/view/login/controller_login/login_controller.dart';
 import 'package:get_it/get_it.dart';
 
 class InjectDependency {
@@ -11,6 +12,8 @@ class InjectDependency {
     getIt.registerSingleton<IDataBaseLocal>(
       SqfliteService(),
     );
+
+    getIt.registerFactory<LoginController>(() =>  LoginController(getIt()));
 
     getIt.registerFactory<HomeController>(() => HomeController());
 
